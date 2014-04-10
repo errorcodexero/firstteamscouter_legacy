d91ca0664ce16b0f09a9c125c14560188cf19fef
tom.sommerville <tom.sommerville@gmail.com>
2014-04-09 22:01:37 -0700

    Added SimpleFTP and Apache Commons libraries. Couldn't get SimpleFTP to work, so will likely remove that in future. Apache library works well, since it sets into Passive mode.
    Can send a file to FTP server. Still need to implement the feature in
    the TeamMatchData::save function.

--- src/com/wilsonvillerobotics/firstteamscouter/ImportMatchDataActivity.java
+++ src/com/wilsonvillerobotics/firstteamscouter/ImportMatchDataActivity.java
@@ -9,10 +9,25 @@ import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.nio.channels.FileChannel;
 import java.util.Hashtable;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.SQLException;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wilsonvillerobotics.firstteamscouter.dbAdapters.MatchDataDBAdapter;
 import com.wilsonvillerobotics.firstteamscouter.dbAdapters.TeamDataDBAdapter;
@@ -20,22 +35,6 @@ import com.wilsonvillerobotics.firstteamscouter.dbAdapters.TeamMatchDBAdapter;
 import com.wilsonvillerobotics.firstteamscouter.utilities.FTPTransfer;
 //import com.wilsonvillerobotics.firstteamscouter.BluetoothSetupActivity;
 import com.wilsonvillerobotics.firstteamscouter.utilities.FTSUtilities;
-
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.database.SQLException;
-import android.graphics.Path;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class ImportMatchDataActivity extends Activity {
 
@@ -76,6 +75,12 @@ public class ImportMatchDataActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_import_match_data);
+		
+		/***
+		 * Trying to avoid the NetworkOnMainThread exception for now...
+		 */
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
 		
 		this.numTestMatches = 25;
 		
@@ -218,27 +223,34 @@ public class ImportMatchDataActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
+				FTSUtilities.printToConsole("ImportMatchDataActivity::btnFTPSend::onClick");
 				String ftpServer = "10.0.0.191";
-				String uName = "toms";
+				String uName = "TomS";
 				String pwd = "arwen";
 				
-				FTPTransfer ftp = new FTPTransfer(ftpServer, uName, pwd);
+				FTPTransfer ftp = new FTPTransfer(getBaseContext(), ftpServer, uName, pwd);
 				try {
 					myTestFile.createNewFile();
 					String header = FTSUtilities.getCSVHeaderString();
-					FileOutputStream fo = new FileOutputStream(myExportFile, false);
+					FileOutputStream fo = new FileOutputStream(myTestFile, false);
 					fo.write(header.getBytes());
 					fo.close();
 					
-					ftp.upload(myTestFile.getName(), myTestFile);
+					ftp.upload(myTestFile);
 					
 				} catch (FileNotFoundException e) {
+					Toast.makeText(getBaseContext(), "FileNotFoundException in onClick", Toast.LENGTH_LONG).show();
+					FTSUtilities.printToConsole("ImportMatchDataActivity::btnFTPSend::onClick : FileNotFoundException");
 					e.printStackTrace();
 				} catch (MalformedURLException e) {
+					Toast.makeText(getBaseContext(), "MalformedURLException in onClick", Toast.LENGTH_LONG).show();
+					FTSUtilities.printToConsole("ImportMatchDataActivity::btnFTPSend::onClick : MalformedURLException");
 					e.printStackTrace();
 				} catch (IOException e) {
+					Toast.makeText(getBaseContext(), "IOException in onClick", Toast.LENGTH_LONG).show();
+					FTSUtilities.printToConsole("ImportMatchDataActivity::btnFTPSend::onClick : IOException");
 					e.printStackTrace();
-				}				
+				}
 			}
 		});
 		
@@ -277,7 +289,6 @@ public class ImportMatchDataActivity extends Activity {
 							try {
 								fi = new FileReader(tempFile);
 							} catch (FileNotFoundException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
 							BufferedReader bufferedReader = new BufferedReader(fi);
@@ -294,7 +305,6 @@ public class ImportMatchDataActivity extends Activity {
 									if(entryNum > 1023) break;
 								}
 							} catch (IOException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
 						}
@@ -322,7 +332,6 @@ public class ImportMatchDataActivity extends Activity {
 						fo.write(header.getBytes());
 						fo.close();
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					
@@ -339,10 +348,8 @@ public class ImportMatchDataActivity extends Activity {
 							}
 							fo.close();
 						} catch (FileNotFoundException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} catch (IOException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 						
--- src/com/wilsonvillerobotics/firstteamscouter/utilities/FTPTransfer.java
+++ src/com/wilsonvillerobotics/firstteamscouter/utilities/FTPTransfer.java
@@ -4,101 +4,181 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.SocketException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 
-public class FTPTransfer {
+import org.jibble.simpleftp.*;
+import org.apache.commons.net.ftp.*;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+public class FTPTransfer extends AsyncTask<File, Integer, Integer> {
+
+	Context ctx;
 	String ftpServerNameOrIP;
 	String ftpUserName;
 	String ftpPassword;
+	SimpleFTP ftp;
 	
-	public FTPTransfer(String ftpServer, String uName, String pwd) {
+	public FTPTransfer(Context ctx, String ftpServer, String uName, String pwd) {
+		this.ctx = ctx;
 		this.ftpServerNameOrIP = ftpServer;
 		this.ftpUserName = uName;
 		this.ftpPassword = pwd;
 	}
 	
-   /**
-    * Upload a file to a FTP server. A FTP URL is generated with the
-    * following syntax:
-    * ftp://user:password@host:port/filePath;type=i.
-    *
-    * @param ftpServer , FTP server address (optional port ':portNumber').
-    * @param user , Optional user name to login.
-    * @param password , Optional password for user.
-    * @param fileName , Destination file name on FTP server (with optional
-    *            preceding relative path, e.g. "myDir/myFile.txt").
-    * @param source , Source file to upload.
-    * @throws MalformedURLException, IOException on error.
-    */
-   public void upload(String fileName, File source) throws MalformedURLException, IOException {
-      if (this.ftpServerNameOrIP != null && fileName != null && source != null)
-      {
-         StringBuffer sb = new StringBuffer( "ftp://" );
-         // check for authentication else assume its anonymous access.
-         if (this.ftpUserName != null && this.ftpPassword != null)
-         {
-            sb.append( this.ftpUserName );
-            sb.append( ':' );
-            sb.append( this.ftpPassword );
-            sb.append( '@' );
-         }
-         sb.append( this.ftpServerNameOrIP );
-         sb.append( '/' );
-         sb.append( fileName );
-         /*
-          * type ==&gt; a=ASCII mode, i=image (binary) mode, d= file directory
-          * listing
-          */
-         sb.append( ";type=i" );
+	public Boolean uploadFile(File fileToFTP) {
+		Boolean result = true;
+		try
+		{
+		    //this.ftp = new SimpleFTP();
 
-         BufferedInputStream bis = null;
-         BufferedOutputStream bos = null;
-         try
-         {
-            URL url = new URL( sb.toString() );
-            URLConnection urlc = url.openConnection();
+		    //Toast.makeText(this.ctx, "FTPTransfer::uploadFile : Server: " + this.ftpServerNameOrIP, Toast.LENGTH_SHORT).show();
+		    //FTSUtilities.printToConsole("FTPTransfer::uploadFile : Server: " + this.ftpServerNameOrIP);
+		    // Connect to an FTP server on port 21.
+		    //this.ftp.connect(this.ftpServerNameOrIP, 21, this.ftpUserName, this.ftpPassword);
 
-            bos = new BufferedOutputStream( urlc.getOutputStream() );
-            bis = new BufferedInputStream( new FileInputStream( source ) );
+			this.setupConnection();
+			
+		    // Set binary mode.
+		    this.ftp.bin();
 
-            int i;
-            // read byte by byte until end of stream
-            while ((i = bis.read()) != -1)
-            {
-               bos.write( i );
-            }
-         }
-         finally
-         {
-            if (bis != null)
-               try
-               {
-                  bis.close();
-               }
-               catch (IOException ioe)
-               {
-                  ioe.printStackTrace();
-               }
-            if (bos != null)
-               try
-               {
-                  bos.close();
-               }
-               catch (IOException ioe)
-               {
-                  ioe.printStackTrace();
-               }
-         }
-      }
-      else
-      {
-         System.out.println( "Input not available." );
-      }
+		    this.ftp.stor(fileToFTP);
+		    
+		    // Change to a new working directory on the FTP server.
+		    this.ftp.cwd("testdir");
+
+		    // Upload some files.
+		    this.ftp.stor(fileToFTP);
+
+		    // You can also upload from an InputStream, e.g.
+		    //ftp.stor(new FileInputStream(new File("test.png")), "test.png");
+		    //ftp.stor(someSocket.getInputStream(), "blah.dat");
+
+		    // Quit from the FTP server.
+		    //this.ftp.disconnect();
+		    this.tearDownConnection();
+		}
+		catch (IOException e)
+		{
+			result = false;
+			//Toast.makeText(this.ctx, "FTPTransfer::uploadFile : IOException: " + e.getMessage(), Toast.LENGTH_LONG).show();
+		    FTSUtilities.printToConsole("FTPTransfer::uploadFile : IOException: " + e.getMessage());
+		    e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public Boolean uploadFiles(SimpleFTP ftp, File fileToFTP) {
+		Boolean result = true;
+		try
+		{
+		    //Toast.makeText(this.ctx, "FTPTransfer::uploadFiles : File: " + fileToFTP.getName(), Toast.LENGTH_SHORT).show();
+		    FTSUtilities.printToConsole("FTPTransfer::uploadFiles : File: " + fileToFTP.getName());
+
+		    // Set binary mode.
+		    this.ftp.bin();
+
+		    this.ftp.stor(fileToFTP);
+		    
+		    // Change to a new working directory on the FTP server.
+		    this.ftp.cwd("testdir");
+
+		    // Upload some files.
+		    this.ftp.stor(fileToFTP);
+		}
+		catch (IOException e)
+		{
+			result = false;
+			//Toast.makeText(this.ctx, "FTPTransfer::uploadFiles : IOException: " + e.getMessage(), Toast.LENGTH_LONG).show();
+		    FTSUtilities.printToConsole("FTPTransfer::uploadFiles : IOException: " + e.getMessage());
+		    e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public void setupConnection() {
+		if(this.ftp == null) {
+			this.ftp = new SimpleFTP();
+	
+		    Toast.makeText(this.ctx, "FTPTransfer::setupConnection : Server: " + this.ftpServerNameOrIP, Toast.LENGTH_SHORT).show();
+		    FTSUtilities.printToConsole("FTPTransfer::setupConnection : Server: " + this.ftpServerNameOrIP);
+		    // Connect to an FTP server on port 21.
+		    try {
+				ftp.connect(this.ftpServerNameOrIP, 21, this.ftpUserName, this.ftpPassword);
+			} catch (IOException e) {
+				Toast.makeText(this.ctx, "FTPTransfer::setupConnection : IOException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+			    FTSUtilities.printToConsole("FTPTransfer::setupConnection : Server: " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void tearDownConnection() {
+		//Toast.makeText(this.ctx, "FTPTransfer::tearDownConnection", Toast.LENGTH_SHORT).show();
+	    FTSUtilities.printToConsole("FTPTransfer::tearDownConnection");
+		if(this.ftp != null) {
+			try {
+				this.ftp.disconnect();
+			} catch (IOException e) {
+				//Toast.makeText(this.ctx, "FTPTransfer::tearDownConnection : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+			    FTSUtilities.printToConsole("FTPTransfer::tearDownConnection : " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+	
+   
+   public Boolean upload(File fileToUpload) {
+	   Boolean result = true;
+	   
+	   FTPClient ftpClient = new FTPClient();
+	   
+	   try {
+			ftpClient.connect(InetAddress.getByName(this.ftpServerNameOrIP));
+			ftpClient.login(this.ftpUserName, this.ftpPassword);
+			ftpClient.changeWorkingDirectory("testdir");
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+	   
+			BufferedInputStream buffIn=null;
+			buffIn=new BufferedInputStream(new FileInputStream(fileToUpload));
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.storeFile("test.txt", buffIn);
+			buffIn.close();
+			ftpClient.logout();
+			ftpClient.disconnect();
+	   } catch (FileNotFoundException e) {
+		   result = false;
+		   Toast.makeText(ctx, "FileNotFoundException during upload: " + e.getMessage(), Toast.LENGTH_LONG).show();
+		   FTSUtilities.printToConsole("FTPTransfer::upload : FileNotFoundException!");
+		   e.printStackTrace();
+	   } catch (SocketException e) {
+		   result = false;
+		   Toast.makeText(ctx, "SocketException during upload: " + e.getMessage(), Toast.LENGTH_LONG).show();
+		   FTSUtilities.printToConsole("FTPTransfer::upload : SocketException!");
+		   e.printStackTrace();
+	   } catch (UnknownHostException e) {
+		   result = false;
+		   Toast.makeText(ctx, "UnknownHostException during upload: " + e.getMessage(), Toast.LENGTH_LONG).show();
+		   FTSUtilities.printToConsole("FTPTransfer::upload : UnknownHostException!");
+		   e.printStackTrace();
+	   } catch (IOException e) {
+		   result = false;
+		   Toast.makeText(ctx, "IOException during upload: " + e.getMessage(), Toast.LENGTH_LONG).show();
+		   FTSUtilities.printToConsole("FTPTransfer::upload : IOException!");
+		   e.printStackTrace();
+	   }
+	   
+	   return result;
    }
 	
    /**
@@ -178,4 +258,24 @@ public class FTPTransfer {
          System.out.println( "Input not available" );
       }
    }
+
+	@Override
+	protected Integer doInBackground(File... files) {
+		Integer fileCount = 0;
+		
+		this.setupConnection();
+		for(File f : files) {
+			if(uploadFiles(ftp, f)) fileCount++;
+		}
+		this.tearDownConnection();
+		return fileCount;
+	}
+	
+	protected void onProgressUpdate(Integer... progress) {
+        //setProgressPercent(progress[0]);
+    }
+
+    protected void onPostExecute(Integer result) {
+    	//Toast.makeText(this.ctx, "FTPTransfer::onPostExecute : numFiles Transfered: " + result, Toast.LENGTH_SHORT).show();
+    }
 }
