da1f6f559368181e75c89b8507dd399af9c82ace
Tom Sommerville <tom.sommerville@3dsystems.com>
2015-01-08 14:52:02 -0800

    Changing from an Eclipse-cased project to an AndroidStudio-based project.

--- gen/com/wilsonvillerobotics/firstteamscouter/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.wilsonvillerobotics.firstteamscouter;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
